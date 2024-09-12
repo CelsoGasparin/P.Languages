104a411f78e43cacbb94f2dc431d618e4560fe05
CelsoGasparin <134729228+CelsoGasparin@users.noreply.github.com>
2024-09-12 15:48:14 -0300

    Add files via upload

--- PL/PHP/menuFun.php
+++ PL/PHP/menuFun.php
@@ -1,10 +1,28 @@
 <?php
 
 
-
-
-
-montarMenu("So","it","Actually","works????","guess so");
+function arrayMenu(array $itens){
+    array_unshift($itens,"Sair");
+    $sizeTab = strlen($itens[0]);
+    foreach($itens as $item){
+        $aSize =strlen($item);
+        if($sizeTab < $aSize){
+            $sizeTab = $aSize;
+        }
+    }
+    $sizeTab += 3; 
+    $sizeInte = $sizeTab+4;
+    print str_repeat("=",$sizeInte)."\n";
+    
+    $qtdItens = count($itens);
+    for($i = 1;$i < $qtdItens;$i++){
+        print "[$i-".$itens[$i].str_repeat(" ",$sizeTab - strlen($itens[$i]))."]\n";
+        print str_repeat("=",$sizeInte)."\n";
+        
+    }
+    print "[0-".$itens[0].str_repeat(" ",$sizeTab - strlen($itens[0]))."]\n";
+    print str_repeat("=",$sizeInte)."\n";
+}
 
 
 
@@ -31,57 +49,3 @@ function montarMenu(...$itens){
     print "[0-".$itens[0].str_repeat(" ",$sizeTab - strlen($itens[0]))."]\n";
     print str_repeat("=",$sizeInte)."\n";
 }
-
-
-function arrayMontarMenu(array $itens){
-    array_unshift($itens,"Sair");
-    $sizeTab = strlen($itens[0]);
-    foreach($itens as $item){
-        $aSize =strlen($item);
-        if($sizeTab < $aSize){
-            $sizeTab = $aSize;
-        }
-    }
-    $sizeTab += 3; 
-    $sizeInte = $sizeTab+4;
-    print str_repeat("=",$sizeInte)."\n";
-    
-    $qtdItens = count($itens);
-    for($i = 1;$i < $qtdItens;$i++){
-        print "[$i-".$itens[$i].str_repeat(" ",$sizeTab - strlen($itens[$i]))."]\n";
-        print str_repeat("=",$sizeInte)."\n";
-        
-    }
-    print "[0-".$itens[0].str_repeat(" ",$sizeTab - strlen($itens[0]))."]\n";
-    print str_repeat("=",$sizeInte)."\n";
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
