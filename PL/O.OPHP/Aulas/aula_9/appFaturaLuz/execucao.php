03ff5abcbd51d46aa675f365e636e8eaf18c9f7d
CelsoGasparin <134729228+CelsoGasparin@users.noreply.github.com>
2024-10-24 12:49:13 -0300

    Add files via upload

--- PL/O.OPHP/Aulas/aula_9/appFaturaLuz/execucao.php
+++ PL/O.OPHP/Aulas/aula_9/appFaturaLuz/execucao.php
@@ -1,4 +1,43 @@
 <?php
 
 
+require_once 'model/ConsumidorResid.php';
+require_once 'model/ConsumidorComer.php';
+require_once 'model/ConsumidorIndu.php';
+require_once '/home/omelby/fun/all.php';
+// require_once '/home/lab/fun/menuFun.php';
+
+
+
+
+$consumidor = null;
+$tipos = ['Residencial','Comercial','Industrial'];
+system('clear');
+print"+++Qual seu tipo de Propriedade?+++";
+arrayMenu(false,$tipos);
+while(true){
+    $opValue = readline();
+    $escolha = strtolower($opValue);
+    switch ($escolha) {
+        case 'residencial':
+            $consumidor = new Residencial();
+        break 2;
+        
+        case 'comercial':
+            $consumidor = new Comercial();
+        break 2;
+
+        case 'industrial':
+            $consumidor = new Industrial();
+        break 2;
+
+        default:
+            
+        break;
+    }
+
+
+}
+
+
 
