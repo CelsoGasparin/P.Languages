a292a55740ad0b4f4e658a4f5e8c7a1a90128d81
CelsoGasparin <134729228+CelsoGasparin@users.noreply.github.com>
2025-06-13 12:35:47 -0300

    Add files via upload

---
+++ PL/LPW/appJogo/model/Jogo.php
@@ -0,0 +1,17 @@
+<?php
+
+class Jogo{
+    private $name;
+    private $img;
+    private $plataformas;
+    private $publica;
+    private $gens;
+    private $desenvolvedor;
+    private $publisher;
+    private $price;
+    private $ageRating;
+    private $fileSize;
+
+
+
+}
